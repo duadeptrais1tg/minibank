d57462c017a72678253e2b1fbf13d42955012867
duadeptrais1tg <vietthupham1@gmail.com>
2025-09-14 22:55:24 +0700

    update SavingAccount

--- src/account/SavingsAccount.java
+++ src/account/SavingsAccount.java
@@ -1,6 +1,16 @@
 package account;
 
-public class SavingsAccount extends BankAccount implements {
+import java.time.LocalDateTime;
+
+public class SavingsAccount extends BankAccount implements IInterestBearing {
 	private double annualInterestRate;
-	public SavingsAccount()
+	public SavingsAccount(String accountNumber, String ownerName, double balance, LocalDateTime createdAt, double annuaIInterestRate) {
+		super(accountNumber, ownerName, balance, createdAt);
+		this.annualInterestRate = annuaIInterestRate;
+	}
+	@Override
+	public void ApplyMonthlyInterest() {
+		double tmp = annualInterestRate / 12 / 100;
+		setBalance(getBalance() + getBalance() * tmp);
+	}
 }
